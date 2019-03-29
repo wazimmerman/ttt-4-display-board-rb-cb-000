2098a39847f3577cfbc4a21519e031137968c6a7
William Zimmerman <william.a.zimmerman@gmail.com>
2019-03-29 04:16:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@ board = ["   ", " X ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def display_board(board)
   pipe = "|"
   separator = "-----------"
-  puts "#{board[0]} #{pipe} #{board[1]} #{pipe} #{board[2]}"
+  puts " #{board[0]} #{pipe} #{board[1]} #{pipe} #{board[2]} "
   puts separator
-  puts "#{board[3]} #{pipe} #{board[4]} #{pipe} #{board[5]}"
+  puts " #{board[3]} #{pipe} #{board[4]} #{pipe} #{board[5]} "
   puts separator
-  puts "#{board[6]} #{pipe} #{board[7]} #{pipe} #{board[8]}"
+  puts " #{board[6]} #{pipe} #{board[7]} #{pipe} #{board[8]} "
 end
 display_board(board)
